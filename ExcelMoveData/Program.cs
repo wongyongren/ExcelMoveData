6effec9aa7d14f5a2a8be6c060c7e0843b67720a
wongyongren <wongyongren@hotmail.com>
2021-03-22 14:34:56 +0800

    update for detect null

--- ExcelMoveData/Program.cs
+++ ExcelMoveData/Program.cs
@@ -11,8 +11,9 @@ namespace ExcelMoveData
         {
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
-            //CreateHead();
-            CopyData();
+            CreateHead();
+            //CopyData();
+            DynamicCopyData();
         }
 
         private static void CreateHead()
@@ -91,6 +92,93 @@ namespace ExcelMoveData
             }
 
             package1.SaveAs(file1);
+            package.SaveAs(file);
+        }
+
+        private static void DynamicCopyData()
+        {
+            var file = new FileInfo(fileName: @"C:\Users\user\Documents\Excel\CopyExcel.xlsx");
+            var file1 = new FileInfo(fileName: @"C:\Users\user\Documents\Excel\8T WWT ELCP Panel Quotation @ 8T Fish Farm.xlsx");
+            using var package = new ExcelPackage(file);
+            using var package1 = new ExcelPackage(file1);
+            var worksheet = package.Workbook.Worksheets["WWT ELCP"];
+            var worksheet1 = package1.Workbook.Worksheets["WWT ELCP"];
+
+            //worksheet.Cells["B2"].Value = "A";
+
+            var row1 = 14;
+
+            for (var row = 3; row < 100; row++)
+            {
+                //var contant = worksheet1.Cells[$"B{row1}"].Value.ToString();
+                //var contant1 = worksheet1.Cells[$"C{row1}"].Value.ToString() != null;
+                if ((worksheet1.Cells[$"B{row1}"].Value ?? "").ToString() != "" || (worksheet1.Cells[$"C{row1}"].Value ?? "").ToString() != "")
+                {
+                    worksheet1.Cells[$"A{row1}"].Copy(worksheet.Cells[$"B{row}"]);
+                    worksheet1.Cells[$"B{row1}"].Copy(worksheet.Cells[$"C{row}"]);
+                    worksheet1.Cells[$"C{row1}"].Copy(worksheet.Cells[$"D{row}"]);
+                    worksheet1.Cells[$"D{row1}"].Copy(worksheet.Cells[$"E{row}"]);
+                    worksheet1.Cells[$"E{row1}"].Copy(worksheet.Cells[$"F{row}"]);
+                    worksheet1.Cells[$"F{row1}"].Copy(worksheet.Cells[$"G{row}"]);
+                    worksheet1.Cells[$"G{row1}"].Copy(worksheet.Cells[$"H{row}"]);
+                    worksheet1.Cells[$"J{row1}"].Copy(worksheet.Cells[$"M{row}"]);
+                    worksheet1.Cells[$"K{row1}"].Copy(worksheet.Cells[$"N{row}"]);
+                    worksheet.Column(row).AutoFit();
+                    row1++;
+                    package.SaveAs(file);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            for (var row = 5; row < 58; row++)
+            {
+                worksheet.Cells[$"E{row}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                worksheet.Cells[$"F{row}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                worksheet.Cells[$"G{row}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                worksheet.Cells[$"H{row}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                worksheet.Cells[$"M{row}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                worksheet.Cells[$"N{row}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            }
+            for (var i = 9; i < 15; i++)
+            {
+                worksheet.Column(i).Style.Font.Color.SetColor(Color.Red);
+                worksheet.Column(i).Style.Numberformat.Format = "General";
+            }
+            worksheet.Column(4).Width = 60;
+            worksheet.Column(3).Width = 13;
+            /*            for (var row = 3; row < 58; row++)
+                        {
+                            worksheet1.Cells[$"B{row1}"].Copy(worksheet.Cells[$"C{row}"]);
+                            worksheet1.Cells[$"C{row1}"].Copy(worksheet.Cells[$"D{row}"]);
+                            worksheet1.Cells[$"D{row1}"].Copy(worksheet.Cells[$"E{row}"]);
+                            worksheet1.Cells[$"E{row1}"].Copy(worksheet.Cells[$"F{row}"]);
+                            worksheet1.Cells[$"F{row1}"].Copy(worksheet.Cells[$"G{row}"]);
+                            worksheet1.Cells[$"G{row1}"].Copy(worksheet.Cells[$"H{row}"]);
+                            worksheet1.Cells[$"J{row1}"].Copy(worksheet.Cells[$"M{row}"]);
+                            worksheet1.Cells[$"K{row1}"].Copy(worksheet.Cells[$"N{row}"]);
+                            worksheet.Column(row).AutoFit();
+                            row1++;
+                        }
+
+                        for (var row = 5; row < 58; row++)
+                        {
+                            worksheet.Cells[$"E{row}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                            worksheet.Cells[$"F{row}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                            worksheet.Cells[$"G{row}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                            worksheet.Cells[$"H{row}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                            worksheet.Cells[$"M{row}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                            worksheet.Cells[$"N{row}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                        }
+                        for (var i = 9; i < 15; i++)
+                        {
+                            worksheet.Column(i).Style.Font.Color.SetColor(Color.Red);
+                            worksheet1.Column(i).Style.Numberformat.Format = "General";
+                        }
+                        worksheet.Column(4).Width = 60;
+                        worksheet.Column(3).Width = 13;*/
+            //package1.SaveAs(file1);
             package.SaveAs(file);
         }
     }
